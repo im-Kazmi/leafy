5f570736b834814b1f060d750daebeffaf4b4543
im-Kazmi <abidkazmi729@gmail.com>
2023-12-01 11:37:48 +0500

    add other functions

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,33 +12,38 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<IUser>();
 
   const router = useRouter();
-  const getUser = async () => {
-    try {
-      const clerkUser: IUser = await getClerkUser();
-      setUser(clerkUser);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  getUser();
-
   useEffect(() => {
-    if (user && user?.role !== "admin") {
-      router.push("/");
-    }
-  }, [user, router]);
-  // const { theme }: any = useThemeContext();
+    const fetchData = async () => {
+      try {
+        const clerkUser: IUser = await getClerkUser();
+        setUser(clerkUser);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  if (user && user?.role !== "admin") {
+    // Redirect immediately
+    router.push("/");
+    return null; // Render nothing while redirecting
+  }
+  // const { theme }: any = useThemeContext();
   return (
     <div className={` flex min-h-screen w-full `}>
-      <div className=" max-md:hidden">
-        <DashboardSidebar />
-      </div>
-
-      <div className=" w-full md:ml-60 flex flex-col items-center  ">
-        <DashboardNavbar />
-        <div className="px-10 flex  w-full mt-5">{children}</div>
-      </div>
+      {user && user.role === "admin" ? (
+        <div className="flex w-full">
+          <div className=" max-md:hidden">
+            <DashboardSidebar />
+          </div>
+          <div className=" w-full md:ml-60 flex flex-col items-center  ">
+            <DashboardNavbar />
+            <div className="px-10 flex  w-full mt-5">{children}</div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
