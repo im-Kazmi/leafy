5bb70c75b0925c46e06ef7f1191e0ee273bfcbcb
im-Kazmi <abidkazmi729@gmail.com>
2023-12-02 13:19:10 +0500

    Added google analytics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,12 +2,15 @@
 "use client";
 import Navbar2 from "@/components/shared/Navbar2";
 import { usePathname } from "next/navigation";
-
+import Head from "next/head";
 import React from "react";
+import Script from "next/script";
+import GoogleAnalytics from "@/components/shared/GoogleAnalytics";
 export default function layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   return (
     <main className="flex flex-col w-full min-h-screen bg-neutral-50 dark:bg-neutral-950 gradient">
+      <GoogleAnalytics />
       {!pathname.startsWith("/blog/") && <Navbar2 />}
       <div className="">{children}</div>
     </main>
